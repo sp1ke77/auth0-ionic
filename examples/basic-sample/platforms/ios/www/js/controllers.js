4dffbf060eb6dff1cd0b2f893aeb14d828aac1c0
Martin Gontovnikas <martin@gon.to>
2014-08-12 15:20:45 -0300

    Changed sample

---
+++ examples/basic-sample/platforms/ios/www/js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, auth, $state) {
+  auth.signin({
+    popup: true,
+    standalone: true
+  }, function() {
+    $state.go('tab.dash');
+  }, function(error) {
+    console.log("There was an error logging in", error);
+  });
+})
+
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
--- examples/ionic-app/www/lib/auth0.js/lib/assert_required.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (obj, prop) {
-  if (!obj[prop]) {
-    throw new Error(prop + ' is required.');
-  }
-};
