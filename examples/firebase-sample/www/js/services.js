0d8a97815a9b6721f7448ab76a1f546a39e3db3d
Martin Gontovnikas <martin@gon.to>
2015-02-06 11:40:28 -0800

    Added Firebase example for iOnic

---
+++ examples/firebase-sample/www/js/services.js
@@ -0,0 +1,31 @@
+angular.module('starter.services', ['firebase'])
+
+/**
+ * A simple example service that returns some data.
+ */
+.service('Friends', function($firebase, store, $state) {
+
+  var friendsRef = new Firebase("https://auth0-ionic-sample.firebaseio.com/friends");
+  friendsRef.authWithCustomToken(store.get('firebaseToken'), function(error, auth) {
+    if (error) {
+      // There was an error logging in, redirect the user to login page
+      $state.go('login');
+    }
+  });
+
+  var friendsSync = $firebase(friendsRef);
+  var friends = friendsSync.$asArray();
+
+  this.all = function() {
+    return friends;
+  }
+
+  this.add = function(name) {
+    friends.$add({name: name});
+  }
+
+  this.get = function(id) {
+    return friends.$getRecord(id);
+  }
+
+});
