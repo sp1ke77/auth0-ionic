2f258ae090b4f07b00325c4b92ac18e66722a48d
Annyv2 <annyv2.0@hotmail.com>
2016-02-25 15:21:26 -0430

    Update page1.ts

--- examples/ionic2-auth0-sample/app/pages/page1/page1.ts
+++ examples/ionic2-auth0-sample/app/pages/page1/page1.ts
@@ -16,6 +16,7 @@ export class Page1 {
   public user_name = "";
   public user_email = "";
   public isAuthenticated = false;
+  public userProfile;
 
   constructor(public nav: NavController, public zone:NgZone, public http: Http, public authHttp: AuthHttp) {
 
@@ -27,9 +28,11 @@ export class Page1 {
         if (err) {
           throw new Error(err);
         }
-
-        localStorage.setItem('profile', JSON.stringify(profile));
+		//Save the token
         localStorage.setItem('id_token', id_token);
+		
+		//Save the profile
+        this.userProfile = profile;
 
         this.user_name = profile.name;
         this.user_email = profile.email;
@@ -42,7 +45,6 @@ export class Page1 {
 
   logout() {
     this.zone.run(() => {
-      localStorage.removeItem('profile');
       localStorage.removeItem('id_token');
 	  this.isAuthenticated = false;
     });
